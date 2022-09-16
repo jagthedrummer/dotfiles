489735b2c73a7ccd44839252461b606c5bd7d962
Jeremy Green <jeremy@octolabs.com>
2022-09-16 11:57:20 -0500

    maybe this is how to handle the Brewfile install step

--- brew/install.sh
+++ brew/install.sh
@@ -1,22 +1,22 @@
 #!/bin/sh
 
-echo "starting tmux"
+#echo "starting tmux"
 
-SRC_DIR=$(cd "$(dirname "$0")"; pwd) # The dir containing this script
+#SRC_DIR=$(cd "$(dirname "$0")"; pwd) # The dir containing this script
 
-link_file () {
-  FILE=~/$1
-  if [ -f "$FILE" ]; then
-    echo "$FILE exists - skipping"
-  else
-    echo "$FILE does not exist - linking"
-    set -x # print the following commands as they execute
-    ln -s "$SRC_DIR/$1" $FILE
-    set +x # turn off command printing
-  fi
-}
+#link_file () {
+  #FILE=~/$1
+  #if [ -f "$FILE" ]; then
+    #echo "$FILE exists - skipping"
+  #else
+    #echo "$FILE does not exist - linking"
+    #set -x # print the following commands as they execute
+    #ln -s "$SRC_DIR/$1" $FILE
+    #set +x # turn off command printing
+  #fi
+#}
 
 
-link_file "Brewfile"
+#link_file "Brewfile"
 
 
--- script/install.sh
+++ script/install.sh
@@ -9,3 +9,8 @@ SRC_DIR=$(cd "$(dirname "$0")"; pwd) # The dir containing this script
 "$SRC_DIR/../git/install.sh"
 "$SRC_DIR/../vim/install.sh"
 "$SRC_DIR/../asdf/install.sh"
+
+cd "$SRC_DIR/../brew"
+brew bundle install
+cd "$SRC_DIR"
+
